797753a657ebf6b52083d82137088ca892c62349
Sergio Rosa <scarlosjazz@gmail.com>
2022-02-21 09:32:24 -0500

    hr-worker

---
+++ Microservices/src/main/java/com/sergiorosa/hrworker/MicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.sergiorosa.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+
+}
